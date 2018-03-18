7690358a46767542e3a72f1e8a7c5d8b9de5c5af
ABHIJEET-YADAV <abhijeet1996yadav@gmail.com>
2018-03-18 23:26:46 +0530

    Final commit

--- routes/imagefile.js
+++ routes/imagefile.js
@@ -2,42 +2,26 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
 
-var imageSchema = mongoose.Schema({
-email: {
-type: String,
-},   
- path: {
- type: String,
- required: true,
- trim: true
- },
- originalname: {
- type: String,
- required: true
- }
- 
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.Promise = global.Promise;
+var mongoDB = 'mongodb://127.0.0.1/uploader';
+mongoose.connect(mongoDB);
+
+var imageSchema = new mongoose.Schema({
+  email: {
+    type: String
+    }
 });
- 
- 
+
 var Image = module.exports = mongoose.model('files', imageSchema);
-
-router.getImages = function(callback, limit) {
- 
- Image.find(callback).limit(limit);
-}
- 
- 
-router.getImageById = function(id, callback) {
-  
- Image.findById(id, callback);
- 
-}
- 
-router.addImage = function(image, callback) {
- Image.create(image, callback);
-}
-
+var loc = "";
 var storage = multer.diskStorage({
  destination: function(req, file, cb) {
  cb(null, 'uploads/')
@@ -56,19 +40,63 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', upload.any(), function(req, res, next) {
- 
- res.send(req.files);
+var myData = new Image(req.body);
+var email=req.body.email;
+ myData.save()
+   .then(item => {
+     res.send("Email saved to database");
+
+     let transporter = nodemailer.createTransport({
+      "type": "SMTP",
+      host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+      auth: {
+        user: 'XXXXXX@XXXXX.com',              //Your Email ID
+        pass: 'XXXXXXXXXX'                     //Your Passward
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+      
+    });
+    
+     var mailOptions = {
+      from: 'XXXXXX@XXXXX.com',              //Your Email ID
+      to: email,                             //Recently Uploaded Email ID
+      subject: 'Sending Email using Node.js',
+      text: 'That was easy!',
+      attachments: [{
+        filename: req.files[0].originalname,
+        streamSource: fs.createReadStream(req.files[0].path)
+    }]
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email has been sent successfully');
+      }
+    });
+   })
+   .catch(err => {
+     res.status(400).send("Unable to save email to database");
+   });
 
  var path = req.files[0].path;
  var imageName = req.files[0].originalname;
-
- var imagepath = {};
- imagepath['path'] = path;
- imagepath['originalname'] = imageName;
- router.addImage(imagepath, function(err) {
- 
- });
- 
+ loc = imageName;
 });
 
+router.get('/pre', function(req, res, next) {
+  str1= "uploads/";
+  var location= path.join(str1,loc);
+  Image.findOne({}, {email:1},function(err,data){
+    if(err) res.jason(err);
+    else res.render("previous.ejs",{user:data.email , url:location})
+  });
+ });
+
 module.exports = router;
+
